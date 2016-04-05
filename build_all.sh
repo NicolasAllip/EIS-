3be3415b7b2d498cd4b80e86cbe89489bcfcb917
Gabriel <sormgabriel@gmail.com>
2016-04-05 08:14:12 -0300

    modifique el build-all

--- build_all.sh
+++ build_all.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env sh
 set -e
-cd ejemplo_banco
-bundle install
-bundle exec rake
-cd ..
-cd chopper
-bundle install
-bundle exec rake
-cd ..
-cd stone_paper_scissors_monkey
-bundle exec rake
-cd ..
-cd base_cucumber
+cd tateti
 bundle install
 bundle exec rake
